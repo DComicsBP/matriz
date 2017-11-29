9cc1650d5ede83bad195e395409ad9a810ac74cc
Rafael Fronteira <rafael_fronteira@hotmail.com>
2017-11-29 21:50:45 -0200

    Uma thread para cada posição da matriz

--- src/javaapplication1/Index.java
+++ src/javaapplication1/Index.java
@@ -1,18 +1,19 @@
 package javaapplication1;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Index {
     
-    static long [][] matrizA = new long[3][3];
-    static long [][] matrizB = new long[3][3];
-    static long [][] result = new long[3][3];
-    static char letra = 'A'; //Não enche com isso!!!!!!
+    static int [][] matrizA = new int[3][3];
+    static int [][] matrizB = new int[3][3];
+    static int [][] result = new int[3][3];
+    static char letra = 'A'; //Não encrenca com isso!!!!!!
     
     
-    static long[][] geraMatriz(long matriz[][]) {
+    static int[][] geraMatriz(int matriz[][]) {
         System.out.println("Matriz " + letra);
-        for (long[] m : matriz) {
+        for (int[] m : matriz) {
             for (int j = 0; j < m.length; j++) {
                 m[j] = ThreadLocalRandom.current().nextInt(1, 9);
                 System.out.print(m[j] + "-");
@@ -24,53 +25,50 @@ public class Index {
     }
     
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         matrizA = geraMatriz(matrizA);
         matrizB = geraMatriz(matrizB);
-        
-        MultiplicaMatriz mm = new MultiplicaMatriz(3,3);
-        MatrizRun thread1 = new MatrizRun(mm);
-        
-        try {
-            Thread t1 = new Thread(thread1);
-            
-            t1.start();
-            t1.join();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-       System.out.println("Resultado: ");
-       for (int i = 0; i < matrizA.length; i++) {
-            for (int j = 0; j < matrizB.length; j++) {
-                System.out.print(result[i][j]+" ");
+        ArrayList<Thread> threads = new ArrayList<>();
+
+       for (int linha = 0; linha < matrizA.length; linha++) {
+            for (int coluna = 0; coluna < matrizB.length; coluna++) {
+                MatrizRun t = new MatrizRun(new MultiplicaMatriz(), linha, coluna);
+                threads.add(new Thread(t));
             }
-            System.out.println("");
         }
+       
+        System.out.println("\n");
+        for (int i = 0; i < threads.size(); i++) {
+              threads.get(i).start();
+              System.out.println(threads.get(i).getName()+" => "+threads.get(i).getState());
+              
+              
+              threads.get(i).join();
+              System.out.println(threads.get(i).getName()+" => "+threads.get(i).getState());
+        }
+        
+       
+        System.out.println("\n\nResultado");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result.length; j++) {
+                System.out.print(result[i][j]+"-");
+            }
+            System.out.println(" ");
+        }
+        
+        
+        
     }
     
 }
 
 class MultiplicaMatriz extends Index {
-    private int a;
-    private int b;
-    
-    public MultiplicaMatriz(int i, int j) {
-        this.a = i;
-        this.b = j;
-    }
-    
-    public void multiplica(){
+  
+    public void multiplica(int linha, int coluna){
         int soma = 0;
-        for (int l = 0; l < a; l++) {
-            for (int m = 0; m < b; m++) {
-                for (int i = 0; i < a; i++) {
-                    soma += matrizA[l][i] * matrizB[i][m];
-                }
-                result[l][m] = soma;
-                soma = 0;
-            }
+        for(int x=0; x < matrizA.length; x++){
+            soma += matrizA[linha][x] * matrizB[x][coluna];
         }
+        result[linha][coluna] = soma;
     }
 }
--- src/javaapplication1/MatrizRun.java
+++ src/javaapplication1/MatrizRun.java
@@ -3,14 +3,17 @@ package javaapplication1;
 public class MatrizRun implements Runnable {  
     
     private MultiplicaMatriz matriz;
-            
-    public MatrizRun(MultiplicaMatriz matriz) {
+    private int linha, coluna;
+    
+    public MatrizRun(MultiplicaMatriz matriz, int i ,int c) {
         this.matriz = matriz;
+        this.linha = i;
+        this.coluna = c;
     }
     
     @Override
     public void run() {
-          matriz.multiplica();
-    }
+        matriz.multiplica(linha, coluna);
+    }  
     
 }
