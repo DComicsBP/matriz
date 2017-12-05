69513206de7ba728dc134ffea595b4609fe3ce72
RAFAEL SANTOS FRONTEIRA <RAFAEL SANTOS FRONTEIRA>
2017-12-05 20:22:36 -0200

    save

--- src/javaapplication1/MatrizRun.java
+++ src/javaapplication1/MatrizRun.java
@@ -1,28 +1,19 @@
 package javaapplication1;
 
-public class MatrizRun implements Runnable {
+public class MatrizRun implements Runnable {  
     
+    private MultiplicaMatriz matriz;
     private int linha, coluna;
-    Integer[][] matrizA = new Integer[3][3];
-    Integer[][] matrizB = new Integer[3][3];
-    Integer[][] matrizResultado = new Integer[3][3];
     
-    
-    public MatrizRun(Integer[][] matrizA, Integer[][] matrizB, Integer[][] matrizResultado, int l ,int c) {
-        this.matrizA            = matrizA;
-        this.matrizB            = matrizB;
-        this.matrizResultado    = matrizResultado;
-        this.linha              = l;
-        this.coluna             = c;
+    public MatrizRun(MultiplicaMatriz matriz, int i ,int c) {
+        this.matriz = matriz;
+        this.linha = i;
+        this.coluna = c;
     }
     
     @Override
     public void run() {
-        int temp = 0;
-         for(int x=0; x < matrizA.length; x++){
-            temp += matrizA[linha][x] * matrizB[x][coluna];
-        }
-         matrizResultado[linha][coluna] = temp;
+        matriz.multiplica(linha, coluna);
     }  
     
 }
